e1de8d02ee47df5859cfb78c4da35ea26b415590
TroublemakerSt <bugandcode@gmail.com>
2017-07-28 16:09:25 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-				<header className="header">
-					
-				</header>
+				<Header title="React FilterGram" />
+				
 				<section className="content">
 
 				</section>
