a8cd5e86ddcb244ccf2c6e72a4b549bd14ab8194
TroublemakerSt <bugandcode@gmail.com>
2017-07-28 15:51:23 +0300

    Beginning of development

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+				<header className="header">
+					
+				</header>
+				<section className="content">
+
+				</section>
+      </div>
+    );
+  }
+}
+
+export default App;
